723dccea4cca3e6ed19a970d3c51fdd70aaf3700
Daria.Szmajduch <daria.szmajduch@performgroup.com>
2020-11-02 22:29:17 +0100

    Moving calendar events to json file

--- middleware/calendar.js
+++ middleware/calendar.js
@@ -1,67 +1,12 @@
-const getCalendarEventsList = () => [
-    {
-        event: {
-            daytime: 'Morning',
-            tasks: {
-                monday: 'Work',
-                tuesday: 'Work',
-                wednesday: 'Work',
-                thursday: 'Work',
-                friday: 'Work',
-                saturday: '',
-                sunday: ''
-            }
-        }
-    },
-    {
-        event: {
-            daytime: 'Afternoon',
-            tasks: {
-                monday: 'Meeting',
-                tuesday: 'Gym',
-                wednesday: '',
-                thursday: 'English',
-                friday: 'Gym',
-                saturday: 'English',
-                sunday: ''
-            }
-        }
-    },
-    {
-        event: {
-            daytime: 'Evening',
-            tasks: {
-                monday: '',
-                tuesday: 'Meeting',
-                wednesday: '',
-                thursday: 'Gym',
-                friday: '',
-                saturday: 'Work',
-                sunday: ''
-            }
-        }
-    },
-    {
-        event: {
-            daytime: 'Night',
-            tasks: {
-                monday: '',
-                tuesday: '',
-                wednesday: '',
-                thursday: '',
-                friday: 'Meeting',
-                saturday: '',
-                sunday: ''
-            }
-        }
-    },
-];
+const fs = require('fs');
+
+const getCalendarEventsList = JSON.parse(fs.readFileSync('./data/calendarEvents.json').toString());
 
 const calendarEventsListMiddleware = (req, res, next) => {
     if(!res.locals.partials) {
         res.locals.partials = {};
     }
-    res.locals.partials.calendarEventsListContext = getCalendarEventsList();
+    res.locals.partials.calendarEventsListContext = getCalendarEventsList;
     next();
 };
 
