5c7231f08e46e832b18b8b23079e166e95daee2f
dariaszmajduch <daria.szmajduch@gmail.com>
2020-11-11 22:16:30 +0100

    adding message id

--- handlers.js
+++ handlers.js
@@ -25,14 +25,6 @@ exports.api = {
         });
     },
     addMessage: (req, res) => {
-        const newMessage = {
-            "name": req.body.name,
-            "email": req.body.email,
-            "phone": req.body.phone,
-            "subject": req.body.subject,
-            "message": req.body.message
-        };
-
         fs.readFile(messagesFilePath, (err) => {
             if (err) {
                 res.send({result: 'File does not exist'});
@@ -40,6 +32,15 @@ exports.api = {
             }
 
             let existingMessages = JSON.parse(fs.readFileSync(messagesFilePath).toString());
+            const maxMessagesId = Math.max(...existingMessages.map(o => o.id), 1);
+            const newMessage = {
+                "id": Object.keys(existingMessages).length === 0 ? 1 : maxMessagesId + 1,
+                "name": req.body.name,
+                "email": req.body.email,
+                "phone": req.body.phone,
+                "subject": req.body.subject,
+                "message": req.body.message
+            };
             existingMessages.push(newMessage);
 
             let newMessagesJSON = JSON.stringify(existingMessages);
